3c78692e364e4c37a8415f0ae84772fa42950f15
liushoukun <liushoukun66@gmail.com>
2022-11-17 23:11:10 +0800

    update auth

---
+++ src/Http/Controllers/Login/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RedJasmine\Login\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+trait LoginController
+{
+
+    public function password(Request $request)
+    {
+
+    }
+
+}
