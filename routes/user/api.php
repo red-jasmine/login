0afeb9d8701fb18fd71d5d58c82e6f96235b45e6
shook <liushoukun66@gmial.com>
2023-02-13 17:49:24 +0800

    商品类目添加

--- routes/user/api.php
+++ routes/user/api.php
@@ -6,10 +6,14 @@ use RedJasmine\Support\Helpers\DomainRoute;
 Route::group([
                  'domain'     => DomainRoute::domain('login'),
                  'prefix'     => 'api/user/login',
-                 'middleware' => 'api',
+                 'middleware' => ['api',],
                  'namespace'  => 'RedJasmine\\Login\\Http\\Controllers\\User\\Api'
-             ], function () {
+             ], static function () {
 
+    Route::group([ 'middleware' => [ 'auth:user-api' ] ], static function () {
+
+
+    });
     Route::post('password', 'LoginController@password')->name('user.api.login.password');
 
 });
