38deffd5a25201e42de4fa070ec82def7db71795
liushoukun <liushoukun66@gmail.com>
2023-02-16 16:32:31 +0800

    路由调整

--- routes/user/api.php
+++ routes/user/api.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 use RedJasmine\Support\Helpers\DomainRoute;
 
 Route::group([
-                 'domain'     => DomainRoute::domain('login'),
-                 'prefix'     => 'api/user/login',
-                 'middleware' => ['api',],
+                 'domain'     => DomainRoute::domain('login', true),
+                 'prefix'     => DomainRoute::userApiPrefix('login'),
+                 'middleware' => [ 'api', ],
                  'namespace'  => 'RedJasmine\\Login\\Http\\Controllers\\User\\Api'
              ], static function () {
 
-    Route::group([ 'middleware' => [ 'auth:user-api' ] ], static function () {
+    Route::group([ 'middleware' => [ 'auth:user' ] ], static function () {
 
 
     });
